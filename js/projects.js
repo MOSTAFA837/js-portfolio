68a0745dc635a59687aba05f5f168aea50211878
MOSTAFA837 <mostafamanchester837@gmail.com>
2021-11-14 19:34:43 +0200

    personal Portfolio

---
+++ js/projects.js
@@ -0,0 +1,50 @@
+let projects = [
+    {
+        name: "Apple e-commerce",
+        tags: "#javascript",
+        image: "images/apple.png",
+        link: "https://appleecommerce.netlify.app/"
+    },
+    {
+        name: "Personal Portfolio",
+        tags: "#javascript",
+        image: "images/portfolio_1.png",
+        link: "https://personportfolio.netlify.app/"
+    },
+    {
+        name: "Lamborghini",
+        tags: "#javascript",
+        image: "images/lamborgini.png",
+        link: "https://mostafa837.github.io/Lamborghini/"
+    },
+    {
+        name: "Ease Bank",
+        tags: "#javascript",
+        image: "images/easeBank.png",
+        link: "https://mostafa837.github.io/EaseBank/"
+    },
+    {
+        name: "Filter Category",
+        tags: "#Mini-Projects",
+        image: "images/menu.png",
+        link: "https://mostafa837.github.io/categoryFilter/"
+    },
+    {
+        name: "The Road",
+        tags: "#javascript",
+        image: "images/theRoad.png",
+        link: "https://mostafa837.github.io/TheRoad/"
+    },
+    {
+        name: "Red Store",
+        tags: "#HTML&CSS",
+        image: "images/redStore.png",
+        link: "https://mostafa837.github.io/categoryFilter/"
+    },
+    {
+        name: "Classic Cars",
+        tags: "#javascript",
+        image: "images/classicCars.png",
+        link: "https://mostafa837.github.io/classic-cars/#home"
+    },
+]
---
+++ js/script.js
@@ -0,0 +1,87 @@
+// Navbar
+
+const sections = document.querySelectorAll('section');
+const navbarLinks = document.querySelectorAll('.link')
+
+window.addEventListener('scroll', () => {
+    myFunc();
+})
+
+const myFunc = () => {
+    sections.forEach((section, i) => {
+        if (window.pageYOffset >= section.offsetTop - 40) {
+            navbarLinks.forEach((navLink) => {
+                navLink.classList.remove('active')
+            })
+            navbarLinks[i].classList.add('active')
+        }
+    })
+}
+
+// Toggle Button
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show')
+})
+
+
+// Projects
+const projectContainer = document.querySelector('.project-container');
+
+projects.forEach(project => {
+    projectContainer.innerHTML += `
+        <div class="project-card" data-tags="#all, ${project.tags}">
+            <div class="img-container">
+                <img src="${project.image}" class="img">
+            </div>
+
+            <div class="content">
+                <span class="tags">${project.tags}</span>
+                <a href="${project.link}" target="_blank" class="link">
+                    <h3 class="project-name">${project.name}</h3>
+                    <i class='bx bx-right-arrow-alt'></i>
+                </a>
+            </div>
+        </div>
+    `
+})
+
+
+// Filter
+const filters = document.querySelectorAll('.filter-btn');
+
+filters.forEach(filterBtn => {
+    filterBtn.addEventListener('click', () => {
+        let id = filterBtn.getAttribute('id');
+
+        let projectCards = document.querySelectorAll('.project-card');
+        projectCards.forEach(card => {
+            if (card.getAttribute('data-tags').includes(id)) {
+                card.classList.remove('hide')
+            } else {
+                card.classList.add('hide')
+            }
+        })
+
+        filters.forEach(btn => btn.classList.remove('active'));
+        filterBtn.classList.add('active')
+    })
+})
+
+
+// Contact Form
+const contactBtn = document.querySelector('.btn-contact');
+const firstName = document.querySelector('.first-name');
+const lastName = document.querySelector('.last-name');
+const email = document.querySelector('.email');
+const msg = document.querySelector('.message');
+
+
+// AOS
+AOS.init({
+    offset:80,
+    duration: 2000,
+})
